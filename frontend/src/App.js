2b57f1bb733721123d01062fb1bc47334f89edbe
AyushMainali123 <ayushmainali11@gmail.com>
2021-07-06 18:05:38 +0545

    New commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
-import Userdahboard from "./screens/userdashboard/Userdashboard";
-import Dashboard from "./screens/hospitaldashboard/Dashboard";
+import Userdahboard from "screens/UserDashboardScreen";
+import Dashboard from "screens/HospitaldashboardScreen/Dashboard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginScreen from "./screens/UserScreen/RegisterScreen/UserRegister";
-import RegisterScreen from "./screens/UserScreen/UserLogin/UserLogin";
-import HomeScreen from "./screens/HomeScreen";
-import AboutScreen from "./screens/AboutScreen";
-import ServiceScreen from "./screens/ServiceScreen";
-import DoctorsScreen from "./screens/DoctorsScreen";
-import CovidScreen from "./screens/CovidScreen";
-import HospitalLogin from "./screens/HospitalScreen/HospitalLogin/HospialLogin";
-import HospitalRegister from "./screens/HospitalScreen/HospitalRegister/HospitalRegister";
-import "./App.css";
+import LoginScreen from "screens/UserScreen/RegisterScreen/UserRegister";
+import RegisterScreen from "screens/UserScreen/UserLogin/UserLogin";
+import HomeScreen from "screens/HomeScreen";
+import AboutScreen from "screens/AboutScreen";
+import ServiceScreen from "screens/ServiceScreen";
+import DoctorsScreen from "screens/DoctorsScreen";
+import CovidScreen from "screens/CovidScreen";
+import HospitalLogin from "screens/HospitalScreen/HospitalLogin/HospialLogin";
+import HospitalRegister from "screens/HospitalScreen/HospitalRegister/HospitalRegister";
+import "App.css";
 import styled from "styled-components";
 
 const AppContainer = styled.div`
   max-width: 1600px;
   margin: auto;
 `;
+
+
 function App() {
+  
+  
   return (
-    
     <AppContainer>
       <Router>
-        <Route path="/userDashboard" component={Userdahboard} />
-        <Route path="/hospitalDashboard" component={Dashboard} />
+        <Route path="/:id/userDashboard/" component={Userdahboard} />
+        <Route path="/:id/hospitalDashboard" component={Dashboard} />
         <Route exact path="/" component={HomeScreen} />
         <Route exact path="/about" component={AboutScreen} />
         <Route exact path="/services" component={ServiceScreen} />
